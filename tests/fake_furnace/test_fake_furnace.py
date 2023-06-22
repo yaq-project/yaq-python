8f295a919095fb522e13de4476ecdb3a9812e30e
Blaise Thompson <blaise@untzag.com>
2023-06-22 18:23:49 -0500

    extra assert

--- tests/fake_furnace/test_fake_furnace.py
+++ tests/fake_furnace/test_fake_furnace.py
@@ -19,6 +19,8 @@ def test_ramp():
     c = yaqc.Client(39426)
     c.set_ramp_time(0)
     c.set_position(0)
+    time.sleep(1)
+    assert c.get_position() == 0
     c.set_ramp_time(1)
     c.set_position(100)
     a = c.get_position()
