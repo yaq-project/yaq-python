cd2f0b014fca81745aadab2935d3a91af4c394ac
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2023-06-28 11:10:32 -0500

    more testing
    test transform with negative slope

--- tests/fake_has_transformed_position/test_fake_has_transformed_position.py
+++ tests/fake_has_transformed_position/test_fake_has_transformed_position.py
@@ -2,8 +2,8 @@ import pathlib
 import subprocess
 import sys
 import time
-import math
 import numpy as np
+import toml
 
 import pytest
 
@@ -15,74 +15,53 @@ from yaqd_core import testing
 config = pathlib.Path(__file__).parent / "config.toml"
 
 
-def to_native(t, nr=1):
-    #  n = 0.5*t - 1
-    rel = 0.5 * t
-    return rel + nr
-
-
-def to_transformed(n, nr=1):
-    #  t = 2*(n - 1)
-    rel = n - nr
-    return 2 * rel
-
-
 @testing.run_daemon_entry_point("fake-has-transformed-position", config=config)
 def test_transform():
     c = yaqc.Client(38001)
     c.set_native_reference(1.0)
     val = 3.0
-    assert np.isclose(c.to_native(val), 2.5)
     bb = c.to_transformed(c.to_native(val))
     assert np.isclose(val, bb)
 
 
 @testing.run_daemon_entry_point("fake-has-transformed-position", config=config)
 def test_limits():
-    c = yaqc.Client(38001)
+    for port in [38001, 38002]:
+        c = yaqc.Client(port)
+        config = toml.loads(c.get_config())
 
-    assert np.isclose(c.get_limits(), [1.0, 4.0]).all()
-    assert np.isclose(c.get_native_limits(), [1.5, 3]).all()
+        limits = c.get_limits()
 
-    val = 3.0
-    c.set_position(val)
-    time.sleep(0.10)
-    assert np.isclose(c.get_position(), val)
-    assert np.isclose(c.get_native_position(), to_native(val))
+        for setpoint, actual in zip(
+            [limits[0]-0.5, 0.5*sum(limits), limits[1]+0.5],
+            [limits[0], 0.5*sum(limits), limits[1]]   
+        ):
+            c.set_position(setpoint)
+            time.sleep(0.10)
+            assert np.isclose(c.get_position(), actual)
+            assert np.isclose(c.get_native_position(), c.to_native(actual))
 
-    val = 0.5
-    c.set_position(val)
-    time.sleep(0.10)
-    assert np.isclose(c.get_position(), 1.0)
-
-    val = 4.5
-    c.set_position(val)
-    time.sleep(0.10)
-    assert np.isclose(c.get_position(), 4.0)
-
-    lims1 = c.get_limits()
-    lims2 = c.get_native_limits()
-    A = [c.to_transformed(lims2[0]), c.to_transformed(lims2[1])]
-    assert np.isclose(lims1[0], 1.0)
-    assert np.isclose(lims1[1], 4.0)
-    assert np.isclose(lims1[0], A[0])
-    assert np.isclose(lims1[1], A[1])
+        lims2 = list(map(c.to_transformed, c.get_native_limits()))
+        assert np.isclose(limits, lims2).all()
 
 
 @testing.run_daemon_entry_point("fake-has-transformed-position", config=config)
 def test_change_native_reference():
-    c = yaqc.Client(38001)
+    for port in [38001, 38002]:
+        c = yaqc.Client(port)
+        factor = toml.loads(c.get_config())["factor"]
 
-    t_pos = 2
-    c.set_native_reference(1)
-    c.set_position(t_pos)
-    time.sleep(0.1)
+        midrange = 0.5 * sum(c.get_limits())
 
-    native_position = c.get_native_position()
-    c.set_native_reference(c.get_native_reference() + 1)
+        c.set_native_reference(1)
+        c.set_position(midrange)
+        time.sleep(0.1)
+        native_position = c.get_native_position()
+        assert np.isclose(native_position, c.to_native(midrange))
+        c.set_native_reference(1.5)
 
-    assert native_position == c.get_native_position()
-    assert c.get_position() == to_transformed(to_native(t_pos) - 1)
+        assert np.isclose(native_position, c.get_native_position())
+        assert np.isclose(c.get_position(), c.to_transformed(c.to_native(midrange) - 0.5))
 
 
 @testing.run_daemon_entry_point("fake-has-transformed-position", config=config)
@@ -95,3 +74,10 @@ def test_set_relative():
     time.sleep(0.1)
 
     assert np.isclose(destination, initial + 1.0)
+
+
+if __name__ == "__main__":
+    test_change_native_reference()
+    test_limits()
+    test_set_relative()
+    test_transform()
